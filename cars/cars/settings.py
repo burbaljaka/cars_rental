7379c228e588682d257472e92c9aab6959a1fc67
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-11-25 09:35:15 +0300

    small fixes

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'cars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,]],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
